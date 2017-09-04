70a1f5cd469540d84e70f1c1b058b0812010a302
raflesn@yahoo.com <raflesn@yahoo.com>
2017-09-04 16:33:51 +0700

    revv

--- dbConnect.php
+++ dbConnect.php
@@ -2,9 +2,9 @@
 
 <?php
 
-$db_server="localhost";
-$db_user="root";
-$db_pass="";
+$db_server="192.168.10.33";
+$db_user="rafles";
+$db_pass="12345";
 $db="androidapp";
 
 $con=mysqli_connect($db_server,$db_user,$db_pass,$db);

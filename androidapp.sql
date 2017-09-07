e0e80823cd002494a765180091355a0bb7c7e0f6
raflesngln <raflesngln@gmail.com>
2017-09-07 10:04:24 +0700

    up

---
+++ androidapp.sql
@@ -0,0 +1,69 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.13-MariaDB : Database - androidapp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`androidapp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `androidapp`;
+
+/*Table structure for table `biodata` */
+
+DROP TABLE IF EXISTS `biodata`;
+
+CREATE TABLE `biodata` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `biodata` */
+
+insert  into `biodata`(`id`,`nama`,`alamat`) values (1,'ere','jakarta'),(2,'rafles','ckg'),(4,'budi','kaliderass');
+
+/*Table structure for table `mahasiswa` */
+
+DROP TABLE IF EXISTS `mahasiswa`;
+
+CREATE TABLE `mahasiswa` (
+  `npm` varchar(12) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `kelas` varchar(10) DEFAULT NULL,
+  `sesi` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`npm`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mahasiswa` */
+
+insert  into `mahasiswa`(`npm`,`nama`,`kelas`,`sesi`) values ('100','Rafles','IIA','siang'),('101','Budi','IIA','Pagi'),('102','mira santika','3A','siang'),('200','Rere','5B','Pagi (09.00-11.00 WI'),('300','Andi','5B','Siang (13.00-15.00 W'),('8080','Ratarara','YGH','Pagi (09.00-11.00 WI');
+
+/*Table structure for table `ms_user` */
+
+DROP TABLE IF EXISTS `ms_user`;
+
+CREATE TABLE `ms_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(111) NOT NULL,
+  `username` varchar(55) NOT NULL,
+  `password` varchar(55) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ms_user` */
+
+insert  into `ms_user`(`id`,`fullname`,`username`,`password`) values (1,'rafles nainggolan','rafles','12345'),(2,'ririn','ririn','123'),(4,'mira ajah','mira','1234'),(5,'jcjcu','fuf','cicic'),(6,'martha','martha','1122'),(7,'reren','rere','55555'),(13,'miranda','miranda','112233'),(14,'mawar','mawar','112233');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
